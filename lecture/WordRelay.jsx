387d91cc0629fb296ea591d29911f7e3c43808a6
kiminpyo <rladlsvy2448@naver.com>
2022-03-23 14:08:30 +0900

    hooks and class

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,13 +1,59 @@
-const React = require('react');
-const {Component} = React; // React.Component => Component로 쓸 수 있다.
-class WordRelay extends Component{
-    state = {
-            text:"hello"
-    };
-    render() {
-        return <div>{this.state.text}</div>
-    }
-}
+const React = require('react');;
+const {useState, useRef} = require('react')
 
+const WordRelay = () =>{
+
+    const [word, setWord] = useState('김인표');
+    const [value, setValue] = useState('');
+    const [result, setResult] =useState('');
+    const inputRef = useRef(null)
+    const onSubmit = (e) =>{
+        e.preventDefault();
+
+        console.log(word[word.length-1])
+        if(word[word.length-1] === value[0]){
+
+         /*    setResult(() =>{
+
+                return '정답';
+            }); */
+            setResult('딩동댕')
+           /*  setWord(() => {
+                return value;
+                
+            }) */
+            setWord(value)
+            setValue('')
+            inputRef.current.focus();
+        }else{
+            setWord(value)
+           setResult('땡')
+            setValue('')
+            inputRef.current.focus();
+        }
+    }
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+    
+
+
+    return(
+        <>
+        <div>{word}</div>
+        <form onSubmit={onSubmit}>
+
+        <input
+        ref={inputRef}       
+        type="text"
+         onChange={onChange}
+         value={value}
+         />
+        <button>확인</button>
+        </form>
+        <div>{result}</div>
+        </>
+        )
+}
 module.exports = WordRelay;
-//다른 곳에서 쓸 수 있게 export
